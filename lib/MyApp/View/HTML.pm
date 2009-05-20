8ae168f81b2dfb018712257504f61e266f1c2c45
Kenichi Ishigaki <ishigaki@cpan.org>
2009-05-20 10:13:22 +0900

    added View/HTML

---
+++ lib/MyApp/View/HTML.pm
@@ -0,0 +1,32 @@
+package MyApp::View::HTML;
+
+use Moose;
+extends 'Catalyst::View';
+use Text::MicroTemplate;
+use Path::Extended;
+
+sub process {
+    my ($self, $c) = @_;
+
+    my $output = $self->render($c)) {
+    $c->res->body($output);
+    $c->res->content_type('text/html');
+}
+
+sub render {
+    my ($self, $c) = @_;
+
+    my $file = $c->stash->{template} || 'default';
+    my $template = file('template', $file)->slurp;
+    my $engine = Text::MicroTemplate->new(
+        line_start => '%',
+        tag_start  => '<%',
+        tag_end    => '%>',
+        template   => $template,
+    );
+    return $engine->build->($self, $c)->as_string;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
