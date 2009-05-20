e07efebfb5f241877ef1b6737efef1871fc08e62
Kenichi Ishigaki <ishigaki@cpan.org>
2009-05-20 10:03:13 +0900

    added Controller/Home

---
+++ lib/MyApp/Controller/Home.pm
@@ -0,0 +1,15 @@
+package MyApp::Controller::Home;
+
+use Moose;
+BEGIN { extends 'Catalyst::Controller'; }
+
+sub latest_entries : Chained('/') PathPart('') Args(0) {
+    my ($self, $c) = @_;
+
+    $c->stash->{entries}  = $c->model('DB')->latest_entries(5);
+    $c->stash->{template} = 'home';
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
