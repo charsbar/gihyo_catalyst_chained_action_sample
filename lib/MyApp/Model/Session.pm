e95d73d3b1192eda2913d0cbaa8c4610811cb5b8
Kenichi Ishigaki <ishigaki@cpan.org>
2009-05-20 21:19:44 +0900

    added Model/Session skeleton

---
+++ lib/MyApp/Model/Session.pm
@@ -0,0 +1,26 @@
+package MyApp::Model::Session;
+
+use Moose;
+extends 'Catalyst::Model';
+
+sub get {
+    my ($self, $id) = @_;
+
+    return unless $id;
+
+    my $session = {
+        id   => $id,
+        data => {
+            user_id => 'admin',
+        },
+    };
+
+    return $session;
+}
+
+sub remove {
+    my ($self, $id) = @_;
+
+}
+
+1;
