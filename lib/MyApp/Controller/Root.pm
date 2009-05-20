f49051b5d7cb23df1d71899453fbf9c1eb60a278
Kenichi Ishigaki <ishigaki@cpan.org>
2009-05-20 10:04:27 +0900

    added /default action to Controller/Root

--- lib/MyApp/Controller/Root.pm
+++ lib/MyApp/Controller/Root.pm
@@ -5,6 +5,12 @@ BEGIN { extends 'Catalyst::Controller'; }
 
 __PACKAGE__->config->{namespace} = '';
 
+sub default : Private {
+    my ($self, $c) = @_;
+
+    $c->forward('/error/not_found');
+}
+
 sub end : ActionClass('RenderView') {}
 
 __PACKAGE__->meta->make_immutable;
